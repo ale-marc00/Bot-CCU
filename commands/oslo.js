bd7cc23d465ba315295a607a661df416314a0f0f
ale-marc00 <marconealessio07@gmail.com>
2026-04-14 23:25:12 +0200

    aggiornamento bot

--- commands/oslo.js
+++ commands/oslo.js
@@ -1,7 +1,7 @@
 const {SlashCommandBuilder} = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder().setName("oslo").setDescription("Risponde con Merda!"),
+    data: new SlashCommandBuilder().setName("oslo").setDescription("Risponde con Aura!"),
 
     async execute(interaction) {
         await interaction.reply("Aura");
